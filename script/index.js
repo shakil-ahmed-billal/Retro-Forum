5450f5d0ff64bf80cff799cec6421619b882342b
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-10-05 00:20:41 +0600

    The last item sections from the API have been brought into the browser, each styled differently and appended

--- script/index.js
+++ script/index.js
@@ -4,20 +4,7 @@ const postData = async(search = "") => {
     const data = await res.json()
     display(data.posts);
 }
-/**
- * "id": 101,
-"category": "Comedy",
-"image": "https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg",
-"isActive": true,
-"title": "10 Kids Unaware of Their Costume",
-"author": {
-"name": "John Doe"
-},
-"description": "It is one thing to subject yourself to a costume mishap",
-"comment_count": 560,
-"view_count": 1568,
-"posted_time": 5
- */
+
 const display =(posts) =>{
     console.log(posts);
     const postSection = document.getElementById('post-container');
@@ -26,8 +13,8 @@ const display =(posts) =>{
         const newPosts = document.createElement('div')
         newPosts.innerHTML = `
                       <div class="broder shadow-2xl p-8 md:flex rounded-3xl">
-                <div class="w-20 h-20 flex relative justify-center md:justify-start">
-                  <img class="w-full rounded-2xl object-cover" src="${post.image}" alt=" ">
+                <div class=" flex relative justify-center md:justify-start">
+                  <img class="w-20 h-20 rounded-2xl object-cover" src="${post.image}" alt=" ">
                   <div class="absolute bg-green-500 w-3 h-3 rounded-full -top-1 -right-1"></div>
                 </div>
                 <div class="ml-6">
@@ -82,4 +69,66 @@ document.getElementById('searchPostsBtn').addEventListener('click', ()=>{
     
 })
 
+
+
+const latestPost = async() =>{
+  const url = `https://openapi.programming-hero.com/api/retro-forum/latest-posts`;
+  const res = await fetch(url)
+  const data = await res.json()
+
+  latestDisplay(data)
+}
+const latestDisplay = (data) =>{
+  const latestSection = document.getElementById('latest-post-container');
+
+  data.forEach(e => {
+
+    const div = document.createElement("div");
+    div.innerHTML= `
+             <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+          <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+              <img
+                  src=${e.cover_image}
+                  alt="Shoes"
+                  class="rounded-xl"
+              />
+          </figure>
+          <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+              <p class="opacity-50 text-start">
+                  <i class="fa-solid fa-calendar-days me-2"></i>author?.posted_date || "No Publish Date"
+              </p>
+              <h2 class="card-title text-start">title</h2>
+              <p class="text-start">
+                  description
+              </p>
+              <div class="card-actions flex gap-5 items-center">
+                  <div class="avatar">
+                      <div
+                          class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                      >
+                          <img
+                          src=${e.profile_image}
+                          />
+                      </div>
+                  </div>
+              <div>
+              <h3 class="text-start font-extrabold">author.name</h3>
+              <p class="text-start opacity-60">author?.designation || "Unknown"</p>
+          </div>
+      </div>
+
+          <span
+            id="latestPostLoader"
+            class="loading loading-infinity loading-lg lg:mt-24 text-primary hidden"
+          >
+
+        </span>
+          <!-- dynamic content -->
+        </div>
+    `
+    latestSection.appendChild(div)
+  });
+}
+
 postData()
+latestPost()
